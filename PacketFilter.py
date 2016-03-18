e7cfaba2079b8768992d1f6bbfb1f0221c1a5cb7
Irvin H <irvinhomem@gmail.com>
2016-03-18 22:10:44 +0100

    Backing up changes ...

---
+++ PacketFilter.py
@@ -0,0 +1,35 @@
+from scapy.all import *
+from scapy_http import *
+
+class PacketFilter(object):
+
+    def __init__(self, filter_subj):
+        self.filter_subj_cap = filter_subj
+
+    def contains(self, filter_str):
+        '''
+
+        :param filter_str:
+        :return:
+        '''
+
+    def comparison(self, comp_subj, comp_operator, comp_obj_string):
+        '''
+
+        :param comp_subj:
+        :param comp_operator:
+        :param comp_obj_string:
+        :return:
+        '''
+
+    def protocolFilter(self):
+        '''
+
+        :return:
+        '''
+
+    def protocol_field_filter(self):
+        '''
+
+        :return:
+        '''
